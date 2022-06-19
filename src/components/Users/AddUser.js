c0ec4f55612b68d683f00c9326aff5aaaa1a732c
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2022-06-19 18:00:27 -0400

    Reusable Card, Button component Validation & Resetting logic

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Card from "./../UI/Card";
+import Button from "./../UI/Button";
+import classes from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const addUserHandler = (e) => {
+    e.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+
+    if (+enteredAge < 1) {
+      return;
+    }
+    console.log(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          value={enteredUsername}
+          type="text"
+          onChange={usernameChangeHandler}
+        />
+        <label htmlFor="username">Age (Years)</label>
+        <input
+          id="username"
+          value={enteredAge}
+          type="text"
+          onChange={ageChangeHandler}
+        />
+        {/* <button type="submit">Add User</button> */}
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
